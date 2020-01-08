ba7d34c5468d555708a09b673973a3b9728d5fb0
Kirya1243 <bukhalov.kirill2003@gmail.com>
2020-01-08 19:20:43 +0500

    Commit 1

--- 1243.py
+++ 1243.py
@@ -1,8 +1,49 @@
-def main():
-    print('My first git program')
-    print('And I change it every day')
-    print('Hello, my name is GreenSMod')
-    print('Hi, my name is Kirya1243')
+import pygame
+import os
+import pygame.mouse
 
-if __name__ == '__main__':
-    main()
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    image = pygame.image.load(fullname).convert()
+    if colorkey is not None:
+        if colorkey == -1:
+            color_key = image.get_at((0, 0))
+        image.set_colorkey(color_key)
+    else:
+        image = image.convert_alpha()
+    return image
+
+
+pygame.init()
+size = 960, 540
+x = 0
+y = 460
+screen = pygame.display.set_mode(size)
+screen.fill((255, 255, 255))
+screen.blit(load_image("Raul.png"), (x, y))
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == 273:
+                y -= 10
+                screen.fill((255, 255, 255))
+                screen.blit(load_image("Raul.png"), (x, y))
+            if event.key == 274:
+                y += 10
+                screen.fill((255, 255, 255))
+                screen.blit(load_image("Raul.png"), (x, y))
+            if event.key == 275:
+                x += 10
+                screen.fill((255, 255, 255))
+                screen.blit(load_image("Raul.png"), (x, y))
+            if event.key == 276:
+                x -= 10
+                screen.fill((255, 255, 255))
+                screen.blit(load_image("Raul.png"), (x, y))
+    pygame.display.flip()
+# завершение работы:
+pygame.quit()
