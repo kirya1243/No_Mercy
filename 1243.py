57945e2d00618e49ca12ac699fea8d1a5224be32
Kirya1243 <bukhalov.kirill2003@gmail.com>
2020-01-08 13:40:55 +0500

    4-ый Commit

--- 1243.py
+++ 1243.py
@@ -2,7 +2,7 @@ def main():
     print('My first git program')
     print('And I change it every day')
     print('Hello, my name is GreenSMod')
-
+    print('Hi, my name is Kirya1243')
 
 if __name__ == '__main__':
     main()
