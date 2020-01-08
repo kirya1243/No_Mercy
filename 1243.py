71ffc179dcbe504796964aab5589b6ae42ae8154
Kirya1243 <bukhalov.kirill2003@gmail.com>
2020-01-09 00:24:29 +0500

    Commit 3

--- 1243.py
+++ 1243.py
@@ -19,12 +19,15 @@ pygame.init()
 size = 960, 540
 x = 0
 y = 460
+v = 5
 screen = pygame.display.set_mode(size)
 screen.fill((255, 255, 255))
-screen.blit(load_image("Raul_r.png"), (x, y))
-raul = "Raul_r.png"
+screen.blit(load_image("Raul_r0.png"), (x, y))
+raul = "Raul_r0.png"
+clock = pygame.time.Clock()
 running = True
 while running:
+    clock.tick(7)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -39,14 +42,31 @@ while running:
                 screen.blit(load_image(raul), (x, y))
             if event.key == 275:
                 x += 10
-                raul = "Raul_r.png"
+                raul = "Raul_r0.png"
                 screen.fill((255, 255, 255))
                 screen.blit(load_image(raul), (x, y))
             if event.key == 276:
                 x -= 10
-                raul = "Raul_l.png"
+                raul = "Raul_l0.png"
                 screen.fill((255, 255, 255))
                 screen.blit(load_image(raul), (x, y))
+    if raul[6] == "0":
+        raul = raul[:6] + '1' + raul[6 + 1:]
+    elif raul[6] == "1":
+        screen.blit(load_image(raul), (x, y))
+        raul = raul[:6] + '2' + raul[6 + 1:]
+    elif raul[6] == "2":
+        screen.blit(load_image(raul), (x, y))
+        raul = raul[:6] + '3' + raul[6 + 1:]
+    elif raul[6] == "3":
+        screen.blit(load_image(raul), (x, y))
+        raul = raul[:6] + '4' + raul[6 + 1:]
+    elif raul[6] == "4":
+        screen.blit(load_image(raul), (x, y))
+        raul = raul[:6] + '5' + raul[6 + 1:]
+    elif raul[6] == "5":
+        screen.blit(load_image(raul), (x, y))
+        raul = raul[:6] + '0' + raul[6 + 1:]
     pygame.display.flip()
 # завершение работы:
 pygame.quit()
