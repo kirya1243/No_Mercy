cf2c18a4666e2dbbd996fad330893b0b8a4f8ec7
Kirya1243 <bukhalov.kirill2003@gmail.com>
2020-01-09 15:23:32 +0500

    Commit 4

--- 1243.py
+++ 1243.py
@@ -17,56 +17,68 @@ def load_image(name, colorkey=None):
 
 pygame.init()
 size = 960, 540
-x = 0
-y = 460
-v = 5
+x = 5
+y = 455
+speed = 10
+kdvij = 0
+
+isJump = False
+jumpCount = 8
+
 screen = pygame.display.set_mode(size)
-screen.fill((255, 255, 255))
-screen.blit(load_image("Raul_r0.png"), (x, y))
+screen.blit(load_image("bg.jpg"), (0, 0))
 raul = "Raul_r0.png"
-clock = pygame.time.Clock()
+
+
+def DrawWindow():
+    screen.blit(load_image("bg.jpg"), (0, 0))
+    screen.blit(load_image(raul), (x, y))
+    pygame.display.flip()
+# clock = pygame.time.Clock()
 running = True
 while running:
-    clock.tick(7)
+    # clock.tick(20)
+    pygame.time.delay(20)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        if event.type == pygame.KEYDOWN:
-            if event.key == 273:
-                y -= 10
-                screen.fill((255, 255, 255))
-                screen.blit(load_image(raul), (x, y))
-            if event.key == 274:
-                y += 10
-                screen.fill((255, 255, 255))
-                screen.blit(load_image(raul), (x, y))
-            if event.key == 275:
-                x += 10
-                raul = "Raul_r0.png"
-                screen.fill((255, 255, 255))
-                screen.blit(load_image(raul), (x, y))
-            if event.key == 276:
-                x -= 10
-                raul = "Raul_l0.png"
-                screen.fill((255, 255, 255))
-                screen.blit(load_image(raul), (x, y))
-    if raul[6] == "0":
+
+    keys = pygame.key.get_pressed()
+    if (keys[pygame.K_RIGHT] or keys[pygame.K_d]) and x < 907:
+        x += speed
+        raul = "Raul_r0.png"
+    if (keys[pygame.K_LEFT] or keys[pygame.K_a]) and x > 5:
+        x -= speed
+        raul = "Raul_l0.png"
+    if not(isJump):
+        if keys[pygame.K_SPACE]:
+            isJump = True
+            raul = raul[:6] + '0' + raul[6 + 1:]
+    else:
+        if jumpCount >= -8:
+            if jumpCount < 0:
+                y += (jumpCount ** 2) / 2
+            else:
+                y -= (jumpCount ** 2) / 2
+            jumpCount -= 1
+        else:
+            isJump = False
+            jumpCount = 8
+
+    if raul[6] == "0" and kdvij % 2 == 0:
         raul = raul[:6] + '1' + raul[6 + 1:]
-    elif raul[6] == "1":
-        screen.blit(load_image(raul), (x, y))
+    elif raul[6] == "1" and kdvij % 2 == 0:
         raul = raul[:6] + '2' + raul[6 + 1:]
-    elif raul[6] == "2":
-        screen.blit(load_image(raul), (x, y))
+    elif raul[6] == "2" and kdvij % 2 == 0:
         raul = raul[:6] + '3' + raul[6 + 1:]
-    elif raul[6] == "3":
-        screen.blit(load_image(raul), (x, y))
+    elif raul[6] == "3" and kdvij % 2 == 0:
         raul = raul[:6] + '4' + raul[6 + 1:]
-    elif raul[6] == "4":
-        screen.blit(load_image(raul), (x, y))
+    elif raul[6] == "4" and kdvij % 2 == 0:
         raul = raul[:6] + '5' + raul[6 + 1:]
-    elif raul[6] == "5":
-        screen.blit(load_image(raul), (x, y))
+    elif raul[6] == "5" and kdvij % 2 == 0:
         raul = raul[:6] + '0' + raul[6 + 1:]
-    pygame.display.flip()
+    kdvij += 1
+
+    DrawWindow()
 # завершение работы:
 pygame.quit()
