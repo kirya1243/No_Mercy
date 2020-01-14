377bd4449341eec22c0d14ae6fae19387fb2e037
GreenSMod <greensmodofficialside@gmail.com>
2020-01-14 16:32:20 +0500

    Commit 10: HP

--- 1243.py
+++ 1243.py
@@ -20,6 +20,7 @@ size = 1200, 540
 x = 5
 y = 455
 speed = 10
+damage = 0
 kdvij = 0
 
 isJump = False
@@ -35,6 +36,7 @@ screen.blit(load_image("bg.jpg"), (0, 0))
 raul = "Raul_r0.png"
 wright = ["Raul_r1b.png", "Raul_r2b.png", "Raul_r3b.png", "Raul_r2b.png"]
 wleft = ["Raul_l1b.png", "Raul_l2b.png", "Raul_l3b.png", "Raul_l2b.png"]
+
 
 def DrawWindow():
     global animCount, raul, kdvij, flstay, left, right, stay
@@ -69,8 +71,19 @@ def DrawWindow():
             raul = raul[:6] + '0' + raul[6 + 1:]
         kdvij += 1
 
+    if isJump:
+        if raul[5] == "r":
+            raul = "Raul_r0.png"
+        else:
+            raul = "Raul_l0.png"
     screen.blit(load_image(raul), (x, y))
+    screen.blit(load_image("Raul_l0.png"), (1147, 455))
+    screen.fill(pygame.Color('red'), pygame.Rect(10, 10, 200, 20))
+    screen.fill(pygame.Color('green'), pygame.Rect(10, 10, 200 - damage, 20))
+    screen.fill(pygame.Color('red'), pygame.Rect(990, 10, 200, 20))
+    screen.fill(pygame.Color('green'), pygame.Rect(990 + damage, 10, 200 - damage, 20))
     pygame.display.flip()
+
 
 clock = pygame.time.Clock()
 running = True
@@ -81,29 +94,38 @@ while running:
             running = False
 
     keys = pygame.key.get_pressed()
-    if (keys[pygame.K_RIGHT] or keys[pygame.K_d]) and x < 907:
+    if (keys[pygame.K_RIGHT] or keys[pygame.K_d]) and x < 1147:
         x += speed
+        if damage < 189:
+            damage += 1
         right = True
         left = False
         stay = False
         flstay = 0
     elif (keys[pygame.K_LEFT] or keys[pygame.K_a]) and x > 5:
         x -= speed
+        if damage < 189:
+            damage += 1
         right = False
         left = True
         stay = False
         flstay = 0
     else:
+        if damage != 0:
+            damage -= 1
         stay = True
         right = False
         left = False
         animCount = 0
-    if not(isJump):
-        if keys[pygame.K_SPACE]:
+    if not (isJump):
+        if keys[pygame.K_w] or keys[pygame.K_UP]:
             isJump = True
-            #raul = raul[:6] + '0' + raul[6 + 1:]
+            if damage < 189:
+                damage += 10
+            # raul = raul[:6] + '0' + raul[6 + 1:]
             stay = False
     else:
+        jump = True
         if jumpCount >= -8:
             if jumpCount < 0:
                 y += (jumpCount ** 2) / 2
