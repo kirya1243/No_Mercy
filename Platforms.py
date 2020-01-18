41a2ccef5b8e829e98bfd130682d5243ef1639de
GreenSMod <greensmodofficialside@gmail.com>
2020-01-19 04:00:08 +0500

    Commit 17.4: Платформа

--- Platforms.py
+++ Platforms.py
@@ -1,10 +1,16 @@
 from pygame.sprite import Sprite
 from pygame.image import load
+from random import randint
+
 
 class Platform(Sprite):
     def __init__(self, x, y):
         Sprite.__init__(self)
-        self.image = load('data/texture.jpg')
+
+        self.texture = load('data/textureMetal.png')
+        self.rx = randint(0, 880)
+        self.ry = randint(0, 880)
+        self.image = self.texture.subsurface((0, 0, 20, 20))
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
