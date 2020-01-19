297424de8a9754a2058d8ea8bed9d6f31dc7a9e4
GreenSMod <greensmodofficialside@gmail.com>
2020-01-19 18:34:29 +0500

    Commit 19: Большое обновление DATA (изображения, звуки и др.)

--- Platforms.py
+++ Platforms.py
@@ -3,11 +3,24 @@ from pygame.image import load
 # from random import randint
 
 
-class Platform(Sprite):
+class PlatformMetal(Sprite):
     def __init__(self, x, y):
         Sprite.__init__(self)
 
-        self.texture = load('data/textureMetal.png')
+        self.texture = load('data/images/texture_metal.png')
+        # self.rx = randint(0, 880)
+        # self.ry = randint(0, 880)
+        self.image = self.texture.subsurface((0, 0, 20, 20))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+
+class PlatformStone(Sprite):
+    def __init__(self, x, y):
+        Sprite.__init__(self)
+
+        self.texture = load('data/images/texture_stone.jpg')
         # self.rx = randint(0, 880)
         # self.ry = randint(0, 880)
         self.image = self.texture.subsurface((0, 0, 20, 20))
@@ -19,7 +32,7 @@ class Platform(Sprite):
 class PlatformHidden(Sprite):
     def __init__(self, x, y):
         Sprite.__init__(self)
-        self.image = load('data/hidden.png')
+        self.image = load('data/images/hidden.png')
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
--- yandex.py
+++ yandex.py
@@ -4,13 +4,77 @@ import pygame.mouse
 from pygame.sprite import Sprite, collide_rect
 from pygame import Surface
 from pygame.image import load
-from Platforms import Platform, PlatformHidden
+from Platforms import PlatformMetal, PlatformStone, PlatformHidden
 
 
 pygame.mixer.init()
-pygame.mixer_music.load('data/sounds/Black Magic.mp3')
+# pygame.mixer_music.load('data/sounds/Black Magic.mp3')
+# pygame.mixer_music.load('data/sounds/Corrupted Keep.mp3')
+# pygame.mixer_music.load('data/sounds/menu.mp3')
 pygame.mixer_music.set_volume(0.3)
-pygame.mixer_music.play(-1)
+
+sound_5_min = pygame.mixer.Sound('data/sounds/game/5_min.wav')
+sound_5_min.set_volume(0.8)
+sound_button_press = pygame.mixer.Sound('data/sounds/game/button_press.wav')
+sound_button_press.set_volume(0.8)
+sound_button_press_game = pygame.mixer.Sound('data/sounds/game/button_press_game.wav')
+sound_button_press_game.set_volume(0.8)
+sound_button_press_map = pygame.mixer.Sound('data/sounds/game/button_press_map.wav')
+sound_button_press_map.set_volume(0.8)
+sound_start_game = pygame.mixer.Sound('data/sounds/game/start_game.wav')
+sound_start_game.set_volume(0.8)
+sound_start_round = pygame.mixer.Sound('data/sounds/game/start_round.wav')
+sound_start_round.set_volume(0.8)
+
+sound_beg = pygame.mixer.Sound('data/sounds/hero/beg.wav')
+sound_beg.set_volume(0.8)
+sound_change_weapon = pygame.mixer.Sound('data/sounds/hero/change_weapon.wav')
+sound_change_weapon.set_volume(0.8)
+sound_death = pygame.mixer.Sound('data/sounds/hero/death.wav')
+sound_death.set_volume(0.8)
+sound_jump_down = pygame.mixer.Sound('data/sounds/hero/jump_down.wav')
+sound_jump_down.set_volume(0.8)
+sound_jump_up = pygame.mixer.Sound('data/sounds/hero/jump_up.wav')
+sound_jump_up.set_volume(0.8)
+sound_railgun_shot = pygame.mixer.Sound('data/sounds/hero/railgun_shot.wav')
+sound_railgun_shot.set_volume(0.8)
+sound_respawn = pygame.mixer.Sound('data/sounds/hero/respawn.wav')
+sound_respawn.set_volume(0.8)
+sound_rifle_shot = pygame.mixer.Sound('data/sounds/hero/rifle_shot.wav')
+sound_rifle_shot.set_volume(0.8)
+
+sound_gain_end = pygame.mixer.Sound('data/sounds/items/gain_end.wav')
+sound_gain_end.set_volume(0.8)
+sound_health_25 = pygame.mixer.Sound('data/sounds/items/health_25_get.wav')
+sound_health_25.set_volume(0.8)
+sound_health_50 = pygame.mixer.Sound('data/sounds/items/health_50_get.wav')
+sound_health_50.set_volume(0.8)
+sound_health_100 = pygame.mixer.Sound('data/sounds/items/health_100_get.wav')
+sound_health_100.set_volume(0.8)
+sound_health_100 = pygame.mixer.Sound('data/sounds/items/health_100_spawn.wav')
+sound_health_100.set_volume(0.8)
+sound_health_ = pygame.mixer.Sound('data/sounds/items/health_spawn.wav')
+sound_health_.set_volume(0.8)
+sound_protection_get = pygame.mixer.Sound('data/sounds/items/protection_get.wav')
+sound_protection_get.set_volume(0.8)
+sound_protection_soon = pygame.mixer.Sound('data/sounds/items/protection_soon.wav')
+sound_protection_soon.set_volume(0.8)
+sound_protection_spawn = pygame.mixer.Sound('data/sounds/items/protection_spawn.wav')
+sound_protection_spawn.set_volume(0.8)
+sound_quad_get = pygame.mixer.Sound('data/sounds/items/quad_get.wav')
+sound_quad_get.set_volume(0.8)
+sound_quad_soon = pygame.mixer.Sound('data/sounds/items/quad_soon.wav')
+sound_quad_soon.set_volume(0.8)
+sound_quad_spawn = pygame.mixer.Sound('data/sounds/items/quad_spawn.wav')
+sound_quad_spawn.set_volume(0.8)
+sound_weapon_get = pygame.mixer.Sound('data/sounds/items/weapon_get.wav')
+sound_weapon_get.set_volume(0.8)
+sound_weapon_spawn = pygame.mixer.Sound('data/sounds/items/weapon_spawn.wav')
+sound_weapon_spawn.set_volume(0.8)
+
+# pygame.mixer_music.play(-1)
+# sound_start_round.play()
+
 
 def load_image(name, colorkey=None):
     fullname = os.path.join('data', name)
@@ -25,7 +89,7 @@ def load_image(name, colorkey=None):
 
 
 def load_level(filename):
-    filename = "data/" + filename
+    filename = "data/tools/" + filename
     with open(filename, 'r') as mapFile:
         level_map = [line.strip() for line in mapFile]
     max_width = max(map(len, level_map))
@@ -35,14 +99,14 @@ def load_level(filename):
 pygame.init()
 size = 1200, 540
 screen = pygame.display.set_mode(size)
-screen.blit(load_image("level_1.jpg"), (0, 0))
+screen.blit(load_image("images/level_1.png"), (0, 0))
 
 
 class Raul(Sprite):
     def __init__(self, x, y):
         Sprite.__init__(self)
         self.image = Surface((48, 80))
-        self.image = load("data/Raul_r0.png").convert_alpha()
+        self.image = load("data/images/Raul/rifle/Raul_r0.png").convert_alpha()
         self.xvel = 0
         self.yvel = 0
         self.rect = self.image.get_rect()
@@ -76,11 +140,11 @@ class Raul(Sprite):
             else:
                 self.sChMarker += 1
             if self.viewSide == 'l' and self.sChMarker == 1:
-                self.image = load("data/" + self.stayL[0]).convert_alpha()
+                self.image = load("data/images/Raul/rifle/" + self.stayL[0]).convert_alpha()
                 self.stayL.insert(6, self.stayL[0])
                 del self.stayL[0]
             elif self.viewSide == 'r' and self.sChMarker == 1:
-                self.image = load("data/" + self.stayR[0]).convert_alpha()
+                self.image = load("data/images/Raul/rifle/" + self.stayR[0]).convert_alpha()
                 self.stayR.insert(6, self.stayR[0])
                 del self.stayR[0]
         else:
@@ -90,7 +154,7 @@ class Raul(Sprite):
             if self.viewSide == 'l':
                 self.rChMarker = 1
                 if self.lChMarker == 1:
-                    self.image = load("data/" + self.begL[0]).convert_alpha()
+                    self.image = load("data/images/Raul/rifle/" + self.begL[0]).convert_alpha()
                     self.begL.insert(4, self.begL[0])
                     del self.begL[0]
                 if self.lChMarker == 4:
@@ -100,7 +164,7 @@ class Raul(Sprite):
             elif self.viewSide == 'r':
                 self.lChMarker = 1
                 if self.rChMarker == 1:
-                    self.image = load("data/" + self.begR[0]).convert_alpha()
+                    self.image = load("data/images/Raul/rifle/" + self.begR[0]).convert_alpha()
                     self.begR.insert(4, self.begR[0])
                     del self.begR[0]
                 if self.rChMarker == 4:
@@ -150,7 +214,7 @@ class Dima(Sprite):
     def __init__(self, x, y):
         Sprite.__init__(self)
         self.image = Surface((48, 80))
-        self.image = load("data/Dima_l0.png").convert_alpha()
+        self.image = load("data/images/Dima/rifle/Dima_l0.png").convert_alpha()
         self.xvel = 0
         self.yvel = 0
         self.rect = self.image.get_rect()
@@ -184,11 +248,11 @@ class Dima(Sprite):
             else:
                 self.sChMarker += 1
             if self.viewSide == 'l' and self.sChMarker == 1:
-                self.image = load("data/" + self.stayL[0]).convert_alpha()
+                self.image = load("data/images/Dima/rifle/" + self.stayL[0]).convert_alpha()
                 self.stayL.insert(6, self.stayL[0])
                 del self.stayL[0]
             elif self.viewSide == 'r' and self.sChMarker == 1:
-                self.image = load("data/" + self.stayR[0]).convert_alpha()
+                self.image = load("data/images/Dima/rifle/" + self.stayR[0]).convert_alpha()
                 self.stayR.insert(6, self.stayR[0])
                 del self.stayR[0]
         else:
@@ -198,7 +262,7 @@ class Dima(Sprite):
             if self.viewSide == 'l':
                 self.rChMarker = 1
                 if self.lChMarker == 1:
-                    self.image = load("data/" + self.begL[0]).convert_alpha()
+                    self.image = load("data/images/Dima/rifle/" + self.begL[0]).convert_alpha()
                     self.begL.insert(4, self.begL[0])
                     del self.begL[0]
                 if self.lChMarker == 4:
@@ -208,7 +272,7 @@ class Dima(Sprite):
             elif self.viewSide == 'r':
                 self.lChMarker = 1
                 if self.rChMarker == 1:
-                    self.image = load("data/" + self.begR[0]).convert_alpha()
+                    self.image = load("data/images/Dima/rifle/" + self.begR[0]).convert_alpha()
                     self.begR.insert(4, self.begR[0])
                     del self.begR[0]
                 if self.rChMarker == 4:
@@ -268,7 +332,11 @@ x, y = 0, 0
 for row in load_level('level_1.txt'):
     for col in row:
         if col == '#':
-            pl = Platform(x, y)
+            pl = PlatformMetal(x, y)
+            sprite_group.add(pl)
+            platfroms.append(pl)
+        elif col == '%':
+            pl = PlatformStone(x, y)
             sprite_group.add(pl)
             platfroms.append(pl)
         elif col == '0':
@@ -303,7 +371,7 @@ class Snaryad:
         self.vel = 30 * facing
 
     def draw(self):
-        screen.blit(load_image("bullet.png"), (self.x, self.y))
+        screen.blit(load_image("images/bullet.png"), (self.x, self.y))
 
 
 class Snaryad1:
@@ -314,11 +382,15 @@ class Snaryad1:
         self.vel1 = 30 * facing1
 
     def draw(self):
-        screen.blit(load_image("bullet1.png"), (self.x, self.y))
+        screen.blit(load_image("images/bullet1.png"), (self.x, self.y))
 
 
 def DrawWindow():
-    screen.blit(load_image("level_1.jpg"), (0, 0))
+    screen.blit(load_image("images/level_1.png"), (0, 0))
+
+    # pygame.draw.rect(screen, (255, 0, 0), [35, 90, 30, 30])
+
+    # screen.blit(pygame.Rect(25, 10, 200, 20))
 
     for bullet in bullets:
         bullet.draw()
